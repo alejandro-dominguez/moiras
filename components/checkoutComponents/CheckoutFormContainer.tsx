e3c5b5570e6fcab09cc1ae72de8c50aaa561bce2
Alejandro Dominguez <alejandro.dominguez1@live.com>
2026-02-11 11:28:13 -0300

    updated checkout styles

--- components/checkoutComponents/CheckoutFormContainer.tsx
+++ components/checkoutComponents/CheckoutFormContainer.tsx
@@ -1,12 +1,21 @@
+import Image from 'next/image';
 import CheckoutForm from './CheckoutForm';
 
 const CheckoutFormContainer = () => {
     return (
-        <div className='grid place-items-center border w-fit mx-auto py-5 px-10'>
-            <h2 className='font-abrli-fatface text-2xl md:text-3xl mb-5'>
-                CHECKOUT
-            </h2>
-            <CheckoutForm />
+        <div className='relative h-[85vh] sm:h-[90vh] grid place-items-center'>
+            <Image
+                src='https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9'
+                className='w-full h-full object-cover object-center'
+                fill
+                alt='asdas'
+            />            
+            <div className='relative z-10 py-5 px-10 bg-[#fffefe] shadow-sm'>
+                <h2 className='font-abrli-fatface text-2xl md:text-3xl mb-5 mt-1'>
+                    CHECKOUT
+                </h2>
+                <CheckoutForm />
+            </div>
         </div>
     )
 }
